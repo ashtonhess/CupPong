e250ec7e666f25c1c79612ff2744d496dbccc664
ashtonhess <ashton.hess@yahoo.com>
2022-03-25 01:13:37 -0500

    Server: adding singleton to handle FileIO class. Using Singleton design pattern here to control writes to the files.

---
+++ v1s/Singleton.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ashton Hess on 3/25/22.
+//
+
+#include "Singleton.h"
+
+Singleton *Singleton::instance=NULL;
+
+Singleton *Singleton::getInstance(){
+    if (!instance){
+        instance = new Singleton();
+    }
+    return instance;
+}
+FileIO Singleton::getFile(){
+    return file;
+}
+void Singleton::setFile(FileIO file) {
+    this->file=file;
+}
---
+++ v1s/Singleton.h
@@ -0,0 +1,33 @@
+//
+// Created by Ashton Hess on 3/25/22.
+//
+
+#ifndef V1_SINGLETON_H
+#define V1_SINGLETON_H
+
+#include <iostream>
+#include "FileIO.h"
+using namespace std;
+class Singleton {
+private:
+    static Singleton *instance;
+    Singleton(){};
+    FileIO file;
+public:
+    static Singleton *getInstance();
+    Singleton(Singleton const& ) = delete; // copy constructor;
+    Singleton& operator=(Singleton const&) = delete; // copy assignment
+    Singleton(Singleton &&) = delete; // move constructor
+    Singleton& operator = (Singleton &&) = delete; // move assignment
+    ~Singleton(); //Deconstructor
+
+    //FileIO ops
+    FileIO getFile();
+    void setFile(FileIO file);
+
+};
+
+
+
+
+#endif //V1_SINGLETON_H
