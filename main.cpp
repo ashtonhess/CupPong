e6f3b6ecf85c1e7d0ef4a10985fa633986a8a272
ashtonhess <ashton.hess@yahoo.com>
2022-03-24 23:10:55 -0500

    Cool welcome message. Prompt for login/signup. socket imports

--- main.cpp
+++ main.cpp
@@ -4,20 +4,69 @@ using namespace std;
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #include "Game.h"
 #include "Cup.h"
 
 
 
-
 int main(int argc, char*argv[]){
 
+    cout<<" ____      ____ ________ _____      ______   ___  ____    ____ ________  \n"<<
+          "|_  _|    |_  _|_   __  |_   _|   .' ___  |.'   `|_   \\  /   _|_   __  | \n"<<
+          "  \\ \\  /\\  / /   | |_ \\_| | |    / .'   \\_/  .-.  \\|   \\/   |   | |_ \\_| \n"<<
+          "   \\ \\/  \\/ /    |  _| _  | |   _| |      | |   | || |\\  /| |   |  _| _  \n"<<
+          "    \\  /\\  /    _| |__/ |_| |__/ \\ `.___.'\\  `-'  _| |_\\/_| |_ _| |__/ | \n"<<
+          " ____\\/__\\/  __|________|________|`.____ .'`.___.|_____||_____|________| \n"<<
+          "                          |  _   _  |.'   `.                                 \n"<<
+          "                          |_/ | | \\_/  .-.  \\                            \n"<<
+          "                              | |   | |   | |                         \n"<<
+          "                             _| |_  \\  `-'  /                           \n"<<
+          "                            |_____|  `.___.'                          \n"<<
+          "   ______ _____  _____ _______    _______    ___  ____  _____  ______    \n"<<
+          " .' ___  |_   _||_   _|_   __ \\  |_   __ \\ .'   `|_   \\|_   _.' ___  |   \n"<<
+          "/ .'   \\_| | |    | |   | |__) |   | |__) /  .-.  \\|   \\ | |/ .'   \\_|   \n"<<
+          "| |        | '    ' |   |  ___/    |  ___/| |   | || |\\ \\| || |   ____   \n"<<
+          "\\ `.___.'\\  \\ \\__/ /   _| |_      _| |_   \\  `-'  _| |_\\   |\\ `.___]  |  \n"<<
+          " `.____ .'   `.__.'   |_____|    |_____|   `.___.|_____|\\____`._____.'   \n"<<
+          "                                                                         ";
+
+    //sleep(3);
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+    cout<<"> Please login or signup."<<endl;
+    cout<<"> Type 'help' for user manual."<<endl;
+
+
+
+
+
+
+
+    //GAME LOOP
+    bool isPlaying=true;
+    while(isPlaying){
+        int x,y;
+        x=0;
+        y=0;
+        if (x-y==1){
+
+
+
+
+
+        }else{
+            isPlaying=false;
+        }
+    }
 
     Game game = Game();
     game.homeCups[0].active=false;// 0 based, homeCups[0]=POINT
     game.awayCups[9].active=false;// 0 based, awayCups[9]=POINT
-    game.printGame();
+    //game.printGame();
 
 
 
