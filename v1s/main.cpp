e9cb1157b4a5087964794f1a710cdc457ccaa6fb
ashtonhess <ashton.hess@yahoo.com>
2022-03-25 01:32:54 -0500

    Server: setting up singleton for FileIO in main.

--- v1s/main.cpp
+++ v1s/main.cpp
@@ -3,10 +3,13 @@
 //
 
 #include <iostream>
+#include "Singleton.h"
 
 
 int main(int argc, char*argv[]){
-
-
-
+    FileIO *f = new FileIO();
+    f->setFilename("users.txt");
+    Singleton::getInstance()->setFile(*f);
+    Singleton*singler = Singleton::getInstance();
+    cout<<"Filename: " <<singler->getFile().getFilename()<<endl;
 }
