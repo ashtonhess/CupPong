6cefd9d4ac41d09267e8817871dbe34c8ac11231
ashtonhess <ashton.hess@yahoo.com>
2022-03-26 00:13:29 -0500

    Client: adding imports and vars to Network class

--- v1c/Network.h
+++ v1c/Network.h
@@ -6,9 +6,26 @@
 #define V1_NETWORK_H
 
 #include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <string>
+#include<vector>
+
+#include <sys/socket.h>
+#include <netinet/in.h>//sockaddr_in
+
+#define SERVER_PORT 11328
+#define MAX_LINE 256
+//#define MAX_PENDING 5
 
 class Network {
-
+private:
+    int sock=0;
+    char* addy="127.0.0.1";//setting addy to the local loopback IP addy
+    struct sockaddr_in sockAddy;
+public:
 };
 
 
