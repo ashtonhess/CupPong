0ebf03b0c525ff4514fbcad0064f33fbf098dbc5
ashtonhess <ashton.hess@yahoo.com>
2022-03-25 23:08:42 -0500

    Server: implementing User class funcs.

--- v1s/User.cpp
+++ v1s/User.cpp
@@ -5,14 +5,14 @@
 #include "User.h"
 
 void User::setUsername(string username){
-
+    username_=username;
 }
 void User::setPassword(string password){
-
+    password_=password;
 }
 string User::getUsername(){
-
+    return username_;
 }
 string User::getPassword(){
-
+    return password_;
 }
