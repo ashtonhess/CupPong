e943ecc02b6021506ba9ec96c7003e91fb00a7af
ahess <ashton.hess@yahoo.com>
2022-03-30 11:45:02 -0500

    Server: -adding ignore for endless loop warning -better prints for accepting 2 connections. -more basic logic worked out for connection process.

--- server/main.cpp
+++ server/main.cpp
@@ -7,6 +7,8 @@
 #include "Singleton.h"
 #include "Network.h"
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
 //comment
 int main(int argc, char*argv[]){
     FileIO *f = new FileIO();
@@ -18,14 +20,13 @@ int main(int argc, char*argv[]){
     Network networkObj = Network();
     bool isActive=false;
 
-    //1 is connected
-    //cout<<"Network connect result: "<<(bool)networkObj.connect()<<endl;
     if(networkObj.connect()){
         isActive=true;
         int sock;
+        int numOfConnections=0;
+        bool acceptingPair=true;
         while(isActive){
-            bool acceptingPair=true;
-            int numOfConnections=0;
+            //store socket connection values for pair
             int s1;
             int s2;
             while (acceptingPair){
@@ -33,31 +34,30 @@ int main(int argc, char*argv[]){
                 if (sock==-1){
                     cout<<"> Error: Network::acceptConnection().";
                     //return -1;//returning
-
                 }else if (sock==-2){
-                    isActive=false;//THIS ELSE IF IS TO MAKE INF LOOP HIGHLIGHT GO AWAY.
+                    isActive=false;//THIS ELSE IF IS TO MAKE INF LOOP HIGHLIGHT GO AWAY IN CLION. NOT ACTUALLY USED.
+                                    //ended up disabling inf loop inspection for this file in CLion. Perm fix.
                 }else{
                     //add this socket to list for matchmaking...
-
                     numOfConnections++;
-                    if(numOfConnections==2){
-                        s2=sock;
-                        cout<<"s2 "<<s2<<endl;
+                    if(numOfConnections==2){//if we have a pair of sockets, stop accepting new connections.
+                        s2=sock;//store value of second socket connected.
+                        cout<<"s2: "<<s2<<endl;
+                        cout<<"Pair of sockets accepted:"<<endl<<"\ts1: "<<s1<<endl<<"\ts2: "<<s2<<endl;
                         acceptingPair=false;
                     }else{
-                        s1=sock;
+                        s1=sock;//store value of first socket connected.
                         cout<<"s1: "<<s1<<endl;
                     }
                     networkObj.sendMsg("Welcome to Cup Pong!\n");
-
-
-
                     //once match is found, add both users into a thread of a game instance...
 
                 }
             }
+            while(true){
 
 
+            }
 
             //isActive=false;
         }
@@ -78,3 +78,5 @@ int main(int argc, char*argv[]){
 
 
 }
+
+#pragma clang diagnostic pop
